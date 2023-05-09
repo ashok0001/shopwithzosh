4f2c58c944ff61a89b2566a82d8d2ced458027fc
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-05-09 20:18:15 +0530

    create handler for change slide

--- src/customer/Components/Home/HomeProductSection.jsx
+++ src/customer/Components/Home/HomeProductSection.jsx
@@ -1,10 +1,17 @@
 import AliceCarousel from "react-alice-carousel";
 import HomeProductCard from "./HomeProductCard";
 import "./HomeProductSection.css";
-import { Button} from "@mui/material";
+import { Button } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useState } from "react";
 
-const HomeProductSection = ({ section,data }) => {
+const HomeProductSection = ({ section, data }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
   const responsive = {
     0: {
       items: 2,
@@ -26,6 +33,10 @@ const HomeProductSection = ({ section,data }) => {
     </div>
   ));
 
+  const slideInFromRight = (t) => {
+    return `translateX(${100 - t * 100}%)`;
+  };
+
   return (
     <div className="relative px-4 sm:px-6 lg:px-8 ">
       <h2 className="text-2xl font-extrabold text-gray-900 py-5">{section}</h2>
@@ -35,44 +46,53 @@ const HomeProductSection = ({ section,data }) => {
           disableDotsControls
           mouseTracking
           items={items}
+          activeIndex={activeIndex}
           responsive={responsive}
+          onSlideChanged={syncActiveIndex}
+          animationType="fadeout"
+          animationDuration={2000}
         />
-        <Button
-          variant="contained"
-          className="z-50 bg-white"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform: "translateX(50%) rotate(90deg)",
-          }}
-       color="white"
-          aria-label="next"
-        >
-          <ArrowForwardIosIcon
-            className="text-black"
-            sx={{ transform: "rotate(-90deg)" }}
-          />
-        </Button>
+        {activeIndex !== items.length - 5 && (
+          <Button
+            onClick={slideNext}
+            variant="contained"
+            className="z-50 bg-white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+            }}
+            color="white"
+            aria-label="next"
+          >
+            <ArrowForwardIosIcon
+              className="text-black"
+              sx={{ transform: "rotate(-90deg)" }}
+            />
+          </Button>
+        )}
 
-        <Button
-          variant="contained"
-          className="z-50 "
-          color="white"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform: "translateX(-50%)  rotate(90deg)",
-          }}
-         
-          aria-label="next"
-        >
-          <ArrowForwardIosIcon
-            className=""
-            sx={{ transform: " rotate(90deg)" }}
-          />
-        </Button>
+        {activeIndex !== 0 && (
+          <Button
+            onClick={slidePrev}
+            variant="contained"
+            className="z-50 "
+            color="white"
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%)  rotate(90deg)",
+            }}
+            aria-label="next"
+          >
+            <ArrowForwardIosIcon
+              className=""
+              sx={{ transform: " rotate(90deg)" }}
+            />
+          </Button>
+        )}
       </div>
     </div>
   );
